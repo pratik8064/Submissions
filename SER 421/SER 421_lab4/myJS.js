a08d33a7973043295fe69a725d72ef0d7dc00cb2
Pratik Suryawanshi <spdpratik17@gmail.com>
2017-10-24 20:25:40 -0700

    update myjs

--- SER 421/SER 421_lab4/myJS.js
+++ SER 421/SER 421_lab4/myJS.js
@@ -17,17 +17,32 @@ function getRandom() {
     var randomRoom = tempRooms[Math.floor(Math.random() * tempRooms.length)];
     var randomSuspect = tempSuspects[Math.floor(Math.random() * tempSuspects.length)];
     var randomWeapon = tempWeapons[Math.floor(Math.random() * tempWeapons.length)];
-    var index = tempRooms.indexOf(randomRoom);
-    tempRooms.splice(index, 1);
-    index = tempSuspects.indexOf(randomSuspect);
-    tempSuspects.splice(index, 1);
-    index = tempWeapons.indexOf(randomWeapon);
-    tempWeapons.splice(index, 1);
     return {
         room: randomRoom,
         suspect: randomSuspect,
         weapon: randomWeapon
     }
+}
+
+function getRandomCompGuess() {
+    console.log(computerGuessList);
+    var randomRoom = computerGuessList.rooms[Math.floor(Math.random() * computerGuessList.rooms.length)];
+    var randomSuspect = computerGuessList.suspects[Math.floor(Math.random() * computerGuessList.suspects.length)];
+    var randomWeapon = computerGuessList.weapons[Math.floor(Math.random() * computerGuessList.weapons.length)];
+    return {
+        room: randomRoom,
+        suspect: randomSuspect,
+        weapon: randomWeapon
+    }
+}
+
+function removeTriplet(player) {
+    var index = tempRooms.indexOf(player.room);
+    tempRooms.splice(index, 1);
+    index = tempSuspects.indexOf(player.suspect);
+    tempSuspects.splice(index, 1);
+    index = tempWeapons.indexOf(player.weapon);
+    tempWeapons.splice(index, 1);
 }
 
 function getPlayerMsg(player, playerName) {
@@ -55,6 +70,7 @@ function assignPlayerCards() {
     var i = 0;
     while (i < len / 2) {
         var temp = getRandom();
+        removeTriplet(temp);
         playerRooms.push(temp.room);
         playerSuspects.push(temp.suspect);
         playerWeapons.push(temp.weapon);
@@ -72,43 +88,41 @@ function assignPlayerCards() {
     };
 }
 
-function getDropdownList(pList, cList){
+function getDropdownList(pList, cList) {
     var res = [];
-    pList.forEach(function(pItem){
-        if(cList.includes(pItem) === false){
+    pList.forEach(function (pItem) {
+        if (cList.includes(pItem) === false) {
             res.push(pItem);
         }
     });
     return res;
 }
 
-function fillDropdowns(){
-    var playerGuessList = {};
+function fillDropdowns() {
+
     playerGuessList.rooms = getDropdownList(rooms, player.rooms);
     playerGuessList.suspects = getDropdownList(suspects, player.suspects);
     playerGuessList.weapons = getDropdownList(weapons, player.weapons);
-    
+
     var roomGuesslement = document.getElementsByName("rooms")[0];
-    for(index in playerGuessList.rooms) {
+    for (index in playerGuessList.rooms) {
         roomGuesslement.options[roomGuesslement.options.length] = new Option(playerGuessList.rooms[index], index);
     }
-    
+
     var suspectGuessElement = document.getElementsByName("suspects")[0];
-    for(index in playerGuessList.suspects) {
+    for (index in playerGuessList.suspects) {
         suspectGuessElement.options[suspectGuessElement.options.length] = new Option(playerGuessList.suspects[index], index);
     }
     var weaponGuessElement = document.getElementsByName("weapons")[0];
-    
-    for(index in playerGuessList.weapons) {
+
+    for (index in playerGuessList.weapons) {
         weaponGuessElement.options[weaponGuessElement.options.length] = new Option(playerGuessList.weapons[index], index);
     }
-    
-    
-    var computerGuessList = {};
+
     computerGuessList.rooms = getDropdownList(rooms, computer.rooms);
     computerGuessList.suspects = getDropdownList(suspects, computer.suspects);
     computerGuessList.weapons = getDropdownList(weapons, computer.weapons);
-    
+
 }
 
 function displayPlayer() {
@@ -121,15 +135,12 @@ function displayPlayer() {
     var element = document.getElementById("usernameForm");
     element.parentNode.removeChild(element);
     pElement[0].innerHTML = getPlayerMsg(player, playerName);
-    
+
     var guessButtonElement = document.getElementById("guessButton");
-    guessButtonElement.removeAttribute('disabled');    
-    //console.log(player);
-    //console.log(secret);
-    //console.log(computer);
-    //console.log(rooms);
-    //console.log(weapons);
-    //console.log(suspects);    
+    guessButtonElement.removeAttribute('disabled');
+    console.log(player);
+    console.log(secret);
+    console.log(computer);
 }
 
 function showTopData() {
@@ -140,61 +151,65 @@ function showTopData() {
 }
 
 //showTopData();
-function checkGuess(){
-    var roomGuessElement = document.getElementsByName("rooms")[0];
-    var selectedRoom = roomGuessElement.options[roomGuessElement.selectedIndex].text;
-    var suspectGuessElement = document.getElementsByName("suspects")[0];
-    var selectedSuspect = suspectGuessElement.options[suspectGuessElement.selectedIndex].text;
-    var weaponGuessElement = document.getElementsByName("weapons")[0];
-    var selectedWeapon = weaponGuessElement.options[weaponGuessElement.selectedIndex].text;
-    
+function checkGuess(flag) {
+    if (flag) {
+        var compGuess = getRandomCompGuess();
+        var selectedRoom = compGuess.room;
+        var selectedSuspect = compGuess.suspect;
+        var selectedWeapon = compGuess.weapon;
+    } else {
+        var roomGuessElement = document.getElementsByName("rooms")[0];
+        var selectedRoom = roomGuessElement.options[roomGuessElement.selectedIndex].text;
+        var suspectGuessElement = document.getElementsByName("suspects")[0];
+        var selectedSuspect = suspectGuessElement.options[suspectGuessElement.selectedIndex].text;
+        var weaponGuessElement = document.getElementsByName("weapons")[0];
+        var selectedWeapon = weaponGuessElement.options[weaponGuessElement.selectedIndex].text;
+    }
+
     var success = false;
-    if(selectedRoom === secret.room){
-        if(selectedSuspect === secret.suspect){
-            if(selectedWeapon === secret.weapon){
+    if (selectedRoom === secret.room) {
+        if (selectedSuspect === secret.suspect) {
+            if (selectedWeapon === secret.weapon) {
                 success = true;
-            }else{
+            } else {
                 wrongGuess = selectedWeapon;
             }
-        }else{
+        } else {
             wrongGuess = selectedSuspect;
         }
-    }else {
+    } else {
         wrongGuess = selectedRoom;
     }
-    
-    var p = document.createElement("p");
-    var input = document.createElement("INPUT");
-    input.type = "button";
-    
-    if(success){
-        var responseMsg = "That was the correct guess! " + selectedSuspect + " did it with the " + selectedWeapon + " in the " + selectedRoom + "!<br>Click to start a new game:  ";
-        input.value = "New Game";
-        input.onClick=function(){
-            restart();   
-        };
-        //input.attachEvent('onclick', restart);
 
-        
-    }else {
-        var responseMsg = "Sorry that was an incorrect guess! The Computer holds the card for " +  wrongGuess + "<br>" + "Click to continue: ";
-        input.value = "Continue";
-        input.onClick = function(){
-            alert('HI');
-            //nextTurn();   
-        };
-        //input.attachEvent('onclick', nextTurn);
+    var p = document.createElement("p");
+    var responseMsg = "";
+
+    if (success && !flag) {
+        responseMsg = "That was the correct guess! " + selectedSuspect + " did it with the " + selectedWeapon + " in the " + selectedRoom + "!<br>Click to start a new game:  <input type=\"button\" value=\"New Game\" onclick=\"restart()\" />";
+    } else if (!success && !flag) {
+        responseMsg = "Sorry that was an incorrect guess! The Computer holds the card for " + wrongGuess + "<br>" + "Click to continue:  <input type=\"button\" value=\"nextTurn\" onclick=\"nextTurn(true)\" />";
+    } else if (success && flag) {
+        responseMsg = "The Computer guessed " + selectedSuspect + " in the " + selectedRoom + " with " + selectedWeapon +
+            "<br>That was correct guess. <br>Click to start a new game:  <input type=\"button\" value=\"New Game\" onclick=\"restart()\" />";
+    } else {
+        "Miss Scarlet in the Library with a Wrench"
+        responseMsg = "The Computer guessed " + selectedSuspect + " in the " + selectedRoom + " with " + selectedWeapon + "<br>The Computer made an incorrect guess! You holds the card for " + wrongGuess + "<br>" + "Click to continue:  <input type=\"button\" value=\"nextTurn\" onclick=\"nextTurn(false)\" />";
     }
     p.innerHTML = responseMsg;
     document.body.appendChild(p);
-    p.appendChild(input);
 }
 
-function nextTurn(){
-    console.log("Its turn for computer");
+function nextTurn(flag) {
+    if (flag) {
+        console.log("Its turn for computer");
+        checkGuess(true);
+    } else {
+        console.log("Its turn for player");
+        checkGuess(false);
+    }
 }
 
-function restart(){
+function restart() {
     console.log("lets start new game");
 }
 
@@ -210,11 +225,15 @@ Click to continue:  [Continue]
 That was the correct guess! Mrs. Green did it with the Candlestick in the Dining Room!
 Click to start a new game:  [New Game]
 */
+function showHistory() {
 
+}
 
 var secret = getRandom();
-console.log("secret : ");
-console.log(secret);
+removeTriplet(secret);
+var playerGuessList = {};
+var computerGuessList = {};
+
 /*var secret = {
     room : "Living Room",
     suspect : "Professor Plum",
