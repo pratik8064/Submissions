63298d50242f6378152ef4e5d4771c52be726c92
Pratik Suryawanshi <spdpratik17@gmail.com>
2017-10-30 00:37:03 -0700

    ser 421 update

--- SER 421/SER 421_lab4/myJS.js
+++ SER 421/SER 421_lab4/myJS.js
@@ -1,13 +1,9 @@
 console.log("-----------start-------------");
 
-/*
+
 var rooms = ["Winterfell", "Kings Landing", "DragonStone", "Vale", "The Wall", "Storms End", "Dorn"];
 var suspects = ["John Snow", "Ned Stark", "Daenerys", "Sam", "Arya", "NightKing", "Jaime Lanister"];
 var weapons = ["Drogon", "Ice", "Oathkeeper", "Heartsbane", "Needle", "Dawn", "LongClow"];
-*/
-var rooms = ["Winterfell", "Kings Landing", "DragonStone"];
-var suspects = ["John Snow", "Ned Stark", "Daenerys"];
-var weapons = ["Drogon", "Ice", "Oathkeeper"];
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -191,17 +187,87 @@ function fillDropdowns() {
 }
 
 function displayPlayer() {
+    //addForm();
     assignPlayerCards();
     fillDropdowns();
     var pElement = document.getElementsByName("displayName");
     var formElement = document.getElementsByName("usernameForm");
     var nameElement = document.getElementsByName("userName");
     var playerName = nameElement[0].value;
+    player.playerName = playerName;
     var element = document.getElementById("usernameForm");
     element.parentNode.removeChild(element);
     pElement[0].innerHTML = getPlayerMsg(player, playerName);
     enableGuess();
     console.log(secret);
+}
+
+function getWinningTime(){
+    var str = "";
+    var d = new Date();
+    str += d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear() + " at " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    return str;
+}
+
+function removeForm(){
+    var form = document.getElementById("guessForm");
+    form.parentNode.removeChild(form);
+}
+
+function addForm(){
+    var form = document.createElement("form");
+    form.id = "guessForm";
+    var label1 = document.createElement("label");
+    label1.innerHTML = "Guess Room:";
+    form.appendChild(label1);
+    var select1 = document.createElement("select");
+    select1.name="rooms";
+    form.appendChild(select1);
+    var br1 = document.createElement("br");
+    form.appendChild(br);
+    
+    var label2 = document.createElement("label");
+    label2.innerHTML = "Guess Suspect:";
+    form.appendChild(label2);
+    var select2 = document.createElement("select");
+    select2.name="suspects";
+    form.appendChild(select2);
+    var br2 = document.createElement("br");
+    form.appendChild(br2);
+    
+    var label3 = document.createElement("label");
+    label3.innerHTML = "Guess Weapon:";
+    form.appendChild(label3);
+    var select3 = document.createElement("select");
+    select3.name="weapons";
+    form.appendChild(select3);
+    var br3 = document.createElement("br");
+    form.appendChild(br3);
+    /*
+    <input id="guessButton" type="button" disabled="true" value="Guess" onclick="checkGuess()" />
+            <input id="historyButton" type="button" value="Show History" />
+            <input id="recordButton" type="button" value="Show Record" onclick="showRecord()" />
+    */
+    var ip1 = document.createElement("input");
+    ip1.id = "guessButton";
+    ip1.type="burron";
+    ip1.disabled = "true";
+    ip1.value = "Guess";
+    ip1.addEventListener('click', checkGuess);
+    form.appendChild(ip1);
+    
+    var ip2 = document.createElement("input");
+    ip2.id = "historyButton";
+    ip2.type="burron";
+    ip2.value = "Show History";
+    ip1.addEventListener('click', showHistory);
+    
+    var ip3 = document.createElement("input");
+    ip3.id = "recordButton";
+    ip3.type="burron";
+    ip3.value = "Show Record";
+    ip3.addEventListener('click', showRecord);
+    
 }
 
 function checkGuess(flag) {
@@ -210,6 +276,9 @@ function checkGuess(flag) {
         var selectedRoom = compGuess.room;
         var selectedSuspect = compGuess.suspect;
         var selectedWeapon = compGuess.weapon;
+        var entry = "Computer guess : [Room - " + selectedRoom + "] [suspect - " + selectedSuspect + "] [weapon - " + selectedWeapon + "] <br>";
+        historyStr += entry;
+        sessionStorage.setItem("historyStr", historyStr);
     } else {
         var roomGuessElement = document.getElementsByName("rooms")[0];
         var selectedRoom = roomGuessElement.options[roomGuessElement.selectedIndex].text;
@@ -217,6 +286,9 @@ function checkGuess(flag) {
         var selectedSuspect = suspectGuessElement.options[suspectGuessElement.selectedIndex].text;
         var weaponGuessElement = document.getElementsByName("weapons")[0];
         var selectedWeapon = weaponGuessElement.options[weaponGuessElement.selectedIndex].text;
+        var entry = "Player guess : [Room - " + selectedRoom + "] [suspect - " + selectedSuspect + "] [weapon - " + selectedWeapon + "] <br>";
+        historyStr += entry;
+        sessionStorage.setItem("historyStr", historyStr);
     }
 
     var success = false;
@@ -242,6 +314,11 @@ function checkGuess(flag) {
         responseMsg = "That was the correct guess! " + selectedSuspect + " did it with the " + selectedWeapon + " in the " + selectedRoom + "!<br>Click to start a new game:  "; 
         continueButton.value="New Game";
         continueButton.addEventListener("click", restart);
+        
+        var recordStr = player.playerName +" vs Computer, "+player.playerName+" won on " + getWinningTime() + " <br>";
+        record += recordStr;
+        localStorage.setItem("record", record);
+        
     } else if (!success && !flag) {
         responseMsg = "Sorry that was an incorrect guess! The Computer holds the card for " + wrongGuess + "<br>" + "Click to continue:  ";
         //<input type=\"button\" value=\"Continue\" onclick=\"checkGuess(true)\" />";
@@ -255,6 +332,9 @@ function checkGuess(flag) {
             "<br>That was correct guess. <br>Click to start a new game:  ";
         continueButton.value="New Game";
         continueButton.addEventListener("click", restart);
+        var recordStr = player.playerName +" vs Computer, Computer won on " + getWinningTime() + " <br>";
+        record += recordStr;
+        localStorage.setItem("record", record);
     } else {
         responseMsg = "The Computer guessed " + selectedSuspect + " in the " + selectedRoom + " with " + selectedWeapon + "<br>The Computer made an incorrect guess! You holds the card for " + wrongGuess + "<br>" + "Click to continue: " 
         disableGuess();
@@ -272,8 +352,52 @@ function checkGuess(flag) {
     document.body.appendChild(p);
 }
 
-function showHistory() {
+function hideHistory(){
+    console.log("hide history");
+    var element = document.getElementById("historyList");
+    if (element !== null)
+        element.parentNode.removeChild(element);
+    var historyButton = document.getElementById("historyButton");
+    historyButton.value = "Show History";
+    historyButton.removeEventListener('click',hideHistory);
+    historyButton.addEventListener('click', showHistory);
+}
 
+function showHistory() {
+    console.log("show history");
+    var historyButton = document.getElementById("historyButton");
+    historyButton.value = "Hide History";
+    historyButton.removeEventListener('click',showHistory);
+    historyButton.addEventListener('click', hideHistory);
+    var historyList = document.createElement("p");
+    historyList.id = "historyList";
+    //var list = "this is new <br> how bou that? <br>";
+    console.log(historyStr);
+    historyList.innerHTML = historyStr;
+    document.getElementById("historyP").appendChild(historyList);
+    
+}
+
+function hideRecord(){
+    console.log("hideRecord");
+    var recordButton = document.getElementById("recordButton");
+    recordButton.value = "Show Record";
+    recordButton.removeEventListener('click',hideRecord);
+    recordButton.addEventListener('click', showRecord);
+    var recordList = document.getElementById("record");
+    recordList.innerHTML = "";
+}
+function showRecord(){
+    console.log("show record");
+    var recordButton = document.getElementById("recordButton");
+    recordButton.value = "Hide Record";
+    recordButton.removeEventListener('click',showRecord);
+    recordButton.addEventListener('click', hideRecord);
+    var recordList = document.getElementById("record");
+    //recordList.id = "historyList";
+    //var list = "this is new <br> how bou that? <br>";
+    recordList.innerHTML = record;
+    //document.getElementById("historyP").appendChild(historyList);
 }
 
 function clearDropdown(name) {
@@ -282,6 +406,7 @@ function clearDropdown(name) {
 }
 
 function restart() {
+    //removeForm();
     var element = document.getElementById("lastPTag");
     if (element !== null)
         element.parentNode.removeChild(element);
@@ -329,7 +454,6 @@ function disableGuess(){
     guessButtonElement.setAttribute("disabled", "true");
 }
 
-showData();
 
 var tempRooms = JSON.parse(JSON.stringify(rooms));
 var tempSuspects = JSON.parse(JSON.stringify(suspects));
@@ -343,6 +467,20 @@ removeTriplet(secret);
 var playerGuessList = {};
 var computerGuessList = {};
 var randomArray = [];
-var pHistory=[];
-var cHistory=[];
-var history = [];
+var historyStr = "";
+var record = " ";
+if (sessionStorage){
+    if(sessionStorage.getItem("historyStr") !== null){
+        historyStr += sessionStorage.getItem("historyStr");
+    }   
+}
+if (localStorage){
+    if(localStorage.getItem("record") !== null){
+        record += sessionStorage.getItem("record");
+    }   
+}
+
+var historyButton = document.getElementById("historyButton");
+historyButton.addEventListener('click', showHistory);
+var recordButton = document.getElementById("recordButton");
+recordButton.addEventListener('click', showRecord);
